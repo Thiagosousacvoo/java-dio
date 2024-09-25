052a444c437d8b9191fb19af63c07e49b1b16760
ThiagoSousa <thiagosousacvoo@gmail.com>
2024-09-25 15:39:38 -0300

    Anatomia das classes

---
+++ meu-primeiro-projeto-java/src/edu/thiago/primeiraSemana/MinhaClasse.java
@@ -0,0 +1,14 @@
+package edu.thiago.primeiraSemana;
+public class MinhaClasse {
+    public static void main (String [] args) {
+
+
+        String meuNome = "Marcos";
+        int minhaIdade = 24;
+        int anoNacimento = 1999;
+
+        meuNome = "Thiago";
+
+        System.out.print("Meu nome é " + meuNome + " tenho " + minhaIdade + " anos e vim ao mundo em " + anoNacimento);
+    }
+}
